b1aa0afad4aec32c50f2f2b197ea0eb7833469c5
shubhamseth29 <shubhamseth29007@gmail.com>
2020-08-27 22:02:18 +0530

    REPO ADDED

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ form.addEventListener('submit',function(e){
     .then((data)=>{
         console.log(data);
         document.getElementById("data").innerHTML=
-        `<div class="card" style="width: 18rem , margin=0px 40%" >
+        `<div class="cardContainer"><div class="card" style="width: 18rem;margin-left:36%">
         <img src="${data.avatar_url}" class="card-img-top" alt="User profile picture">
         <div class="card-body">
           <h5 class="card-title">${data.name}</h5>
@@ -23,10 +23,59 @@ form.addEventListener('submit',function(e){
           <a href="${data.html_url}" class="card-link">View Profile</a>
           <a href= "https://shubhamseth29.github.io/GithubProfileFinder/" class="card-link">Find Another </a>
         </div>
-      </div>`
-
+      </div></div>`
     })
     
-})
+    fetch("https://api.github.com/users/"+username+"/repos")
+    .then((result)=>result.json())
+    .then((repo)=>{
+        console.log(repo)
+        
+
+        let output=`<h2 class="mb-4" id="repoHead">REPOSITORIES</> `
+         repo.forEach((i)=>{
+             output+=
+                    `  <div class="accordion" id="accordionExample">
+                    <div class="card">
+                      <div class="card-header" id="headingOne">
+                        <h2 class="mb-0">
+                          <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                            ${i.name}
+                            </button>
+                            </h2>
+                          </div>
+                      
+                          <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                            <div class="card-body">
+                            <div class="col-md-7">
+                            Description:${i.description}
+                            </div>
+                            <div class="col-md-3">
+                            <span class="badge badge-dark">Forks: ${i.forks_count}</span>
+                            <span class="badge badge-primary">Watchers: ${i.watchers_count}</span>
+                            <span class="badge badge-success">Stars: ${i.stargazers_count}</span>
+                            </div>
+                            <div class="col-md-2">
+                            <a href="${i.html_url}" target="_blank" class="btn btn-dark">Click here to go to the Repo Page</a>
+                            </div>
+                              
+                            </div>
+                          </div>
+                        </div>
+                      </div>`
+
+                })
+            
+
+        document.getElementById("repo").innerHTML=output;
+         
+     
+               
+      
+            
+        })
+
+    })
+
 
 
